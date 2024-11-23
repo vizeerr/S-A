0bffddaf9bf594578a0ae3a27f0309a14b005406
vivek sagar <vivek54540@gmail.com>
2024-11-23 17:18:01 +0530

    first commit v2

--- src/components/Connecting.js
+++ src/components/Connecting.js
@@ -4,7 +4,7 @@ const Connecting = () => {
   return (
     <div className=' md:p-20 p-10 bg-zinc-100 text-center'>
         <h3 className='xl:text-6xl sm:text-4xl text-3xl'>Connecting Experts, Empowering Clients</h3>
-        <p className='xl:text-2xl sm:text-xl text-lg mt-5 lg:w-2/3 w-full mx-auto leading-relaxed text-neutral-600'>At our Legal Service Platform, we're committed to revolutionizing the way you access legal services. Our platform connects you with skilled legal professionals, making it easier than ever to find the expertise you need.</p>
+        <p className='xl:text-2xl sm:text-xl text-lg mt-5 lg:w-2/3 w-full mx-auto leading-relaxed text-neutral-600'>At our Legal Service Platform, were committed to revolutionizing the way you access legal services. Our platform connects you with skilled legal professionals, making it easier than ever to find the expertise you need.</p>
       
     </div>
   )
